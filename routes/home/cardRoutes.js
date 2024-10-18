e9b77bf9cec8ba6b10ee266f9278feb3eba16388
divesh_joshi <diveshjoshi0766@gmail.com>
2024-10-19 00:17:52 +0530

    syncing

--- routes/home/cardRoutes.js
+++ routes/home/cardRoutes.js
@@ -6,7 +6,6 @@ router.get('/home/product/get-card-product/:userId',cardController.get_card_prod
 router.delete('/home/product/delete-card-product/:card_id',cardController.delete_card_products)
 router.put('/home/product/quantity-inc/:card_id',cardController.quantity_inc)
 router.put('/home/product/quantity-dec/:card_id',cardController.quantity_dec)
-
 router.post('/home/product/add-to-wishlist',cardController.add_wishlist) 
 router.get('/home/product/get-wishlist-products/:userId',cardController.get_wishlist) 
 router.delete('/home/product/remove-wishlist-product/:wishlistId',cardController.remove_wishlist) 
